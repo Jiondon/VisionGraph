dd36a3892b0be44b088595fce4404e56f1c3593e
lixiaodong <1973880959@qq.com>
2019-05-22 17:45:27 +0800

    图片缩放测试

--- src/item/visionregion.cpp
+++ src/item/visionregion.cpp
@@ -49,7 +49,7 @@ void VisionRegion::paint(QPainter *painter, const QStyleOptionGraphicsItem *opti
 
     painter->setPen(QPen(m_regionColor,0));  //区域采用填充的颜色，原因自己想
     QVector<QLineF> vecLines;
-    if(m_vecLines.size() <= 0){
+    if(m_vecLines.size() > 0){
         for(int i=0;i<m_vecLines.size();i++){
     //        qDebug()<<this->mapFromScene(m_vecLines.at(i).p1());
             QLineF lineF = QLineF(this->mapFromScene(m_vecLines.at(i).p1()),this->mapFromScene(m_vecLines.at(i).p2()));
@@ -61,7 +61,7 @@ void VisionRegion::paint(QPainter *painter, const QStyleOptionGraphicsItem *opti
 
 
     vector<VGPointRun> vec_point;
-    if(m_vecRegionPair.size() <= 0){
+    if(m_vecRegionPair.size() > 0){
         for(int i=0;i<m_vecRegionPair.size();i++){
             vec_point.clear();
             vec_point = m_vecRegionPair.at(i).region.arrayPointRun;
